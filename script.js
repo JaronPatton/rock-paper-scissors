fbf05d4b41b2ec40c3d0f5813da2f3975e73f489
jaronpatton <jpjaron09@gmail.com>
2022-06-16 21:49:24 -0500

    computerPlay() randomly returns rock, paper, or scizzors

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello friend!")
+
+
+function computerPlay() {
+    const pick = (Math.floor(Math.random() * 3) + 1);
+    switch (pick) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scizzors";
+    }
+}
