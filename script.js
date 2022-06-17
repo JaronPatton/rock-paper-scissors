eef933583cf46fe1f03467f6d45624a706985613
jaronpatton <jpjaron09@gmail.com>
2022-06-16 21:54:26 -0500

    Added function comment for action description

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 console.log("Hello friend!")
 
 
+
+//randomly return "rock", "paper", or "scissors"
 function computerPlay() {
     const pick = (Math.floor(Math.random() * 3) + 1);
     switch (pick) {
@@ -9,6 +11,6 @@ function computerPlay() {
         case 2:
             return "paper";
         case 3:
-            return "scizzors";
+            return "scissors";
     }
 }
